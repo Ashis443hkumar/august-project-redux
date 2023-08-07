9bf992b9c1ffcec88d99c4046f9096278a48a48a
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-08-07 09:16:51 +0530

    2 files modified, 1 added, 1 deleted

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { BrowserRouter } from 'react-router-dom';
+
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+   <Provider store={store}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+   </Provider>
   </StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
